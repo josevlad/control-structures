f1e1f1abed8a202ee14d67a3e0426ab19adc88c1
Vladimir Castañeda <vladimir29castaneda@gmail.com>
2020-07-31 19:37:27 -0300

    finish code exercise-two

--- exercise-two/src/ar/com/ada/online/second/exercisetwo/Main.java
+++ exercise-two/src/ar/com/ada/online/second/exercisetwo/Main.java
@@ -1,8 +1,23 @@
 package ar.com.ada.online.second.exercisetwo;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        int number;
+        String result;
+        Scanner keyboard = new Scanner(System.in);
 
+        System.out.print("Ingrese el numero a evaluar: ");
+        number = keyboard.nextInt();
+
+        if (number >= 0) {
+            result = "Positivo";
+        } else {
+            result = "Negativo";
+        }
+
+        System.out.println("El numero es: " + result);
     }
 }
